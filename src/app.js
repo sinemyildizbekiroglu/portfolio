d457ad56384a4e63fea395b6d54207c73009488c
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-12-14 20:57:37 +0100

    Update `app.js`

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const hamburgerButton = document.getElementById("hamburger")
+const navKist = document.getElementById("nav-list")
+
+function toggleButton() { 
+    navList.classList.toggle("show")
+}
+
+hamburgerButton.addEventListener("click", toggleButton)
+ 
